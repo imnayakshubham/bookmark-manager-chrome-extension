f86ddbd0a6c11c671940d9f92beeb5389f0e07a6
imnayakshubham <imnayakshubham@gmail.com>
2024-12-28 17:32:26 +0530

    fix..

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
+  ssr: {
+    noExternal: ["react-dom"],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
