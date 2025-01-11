e511a200a53e4ffa350ad5db851a6eaf69d42cb5
imnayakshubham <imnayakshubham@gmail.com>
2025-01-11 10:51:39 +0530

    fix: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+// import { crx } from '@crxjs/vite-plugin'
+// import manifest from './manifest.json'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react()],
   ssr: {
     noExternal: true,
   },
